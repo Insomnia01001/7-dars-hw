1d964122aae58a8334bdbfd2d7bd4f94110b4097
Insomnia01001 <167763964+Insomnia01001@users.noreply.github.com>
2024-09-03 13:08:24 +0300

    Add files via upload

---
+++ 7  dars hw/main.py
@@ -0,0 +1,52 @@
+result1 = int(input("1 dan 7 gacham son kiriting: "))
+if result1 == 1:
+        print("Dushanba")
+elif result1 == 2 :
+        print("seshanba")
+elif result1 == 3 : 
+        print("chorshanba")
+elif result1 == 4 :
+        print("payshanba")
+elif result1 == 5 :
+        print("juma")
+elif result1 == 6 :
+        print("shanba ")
+elif result1 == 7 :
+        print("yakshanba ")
+else:
+        print("siz 1 dan 7 gacham son kiritmadingiz: ")
+
+result2 = input("oyni kiriting ")
+if result2 == "yanvar":
+        print("hozir oqsh vaqti")
+elif result2 == "fevral":
+        print("hozir oqsh vaqti")
+elif result2 == "mart":
+        print("hozir oqsh vaqti")
+elif result2 == "aprel":
+        print("hozir oqsh vaqti")
+elif result2 == "may":
+        print("hozir oqsh vaqti")
+elif result2 == "iyun":
+        print("iyun Hozir yozgi tatil")
+elif result2 == "iyul":
+        print("iyul Hozir yozgi tatil")
+elif result2 == "avgust":
+        print("avgust Hozir yozgi tatil")
+elif result2 == "sentabr":
+        print("hozir oqsh vaqti ")
+elif result2 == "oktabr":
+        print("hozir oqsh vaqti")
+elif result2 == "noyabr":
+        print("hozir oqsh vaqti")
+elif result2 == "dekabr":
+        print("hozir oqsh vaqti")
+else:
+        print("siz oyni nomini kiritmadiz")
+result3 = input("svetafor rangini kirirting ")   
+if result3 == "qizil":
+        print("toxtang")    
+if result3 == "sariq":
+        print("tayorlaning")   
+if result3 == "yashil":
+        print("yuring")    
